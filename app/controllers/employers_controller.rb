23833c968875cd279d8fa5978b60ff731589586f
gogojojo <joseph.gochua@gmail.com>
2015-03-26 14:09:01 -0700

    added flash msg

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -2,6 +2,7 @@ class EmployersController < ApplicationController
   def create
     @employer = Employer.new(params.require(:employer).permit(:email))
     if @employer.save
+      flash[:notice] = "An email has been sent to #{@employer.email}"
       UserNotifier.send_signup_email(@employer).deliver
       redirect_to '/' 
     end
